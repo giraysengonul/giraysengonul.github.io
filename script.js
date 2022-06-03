c6656db1db0dcfa45aac50c6d395c26a1bba3aa1
Hakkı Can Şengönül <56843071+hakkicansengonul@users.noreply.github.com>
2022-06-03 21:16:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Engineer","iOS Developer","Flutter Developer", "Blogger", "Designer", "Freelancer","Mobile Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
