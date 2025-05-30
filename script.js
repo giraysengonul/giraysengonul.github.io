081b622968748fe2f0a1b84e6d5653a8f8d1dc03
Giray sengonul <giraysengonul@gmail.com>
2025-05-30 15:33:06 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Engineer","iOS Developer","Flutter Developer", "Blogger", "Designer", "Freelancer","Mobile Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        cursorChar: ' |',
-        smartBackspace: true,
-        loop: true
-    });
